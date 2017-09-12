65fd3e9e6693e8ba0faa0b81dff38420e3fd717a
Wilfred Lan <l337will@live.com>
2017-09-12 19:19:45 +1200

    working results and changed background

--- js/data-get.js
+++ js/data-get.js
@@ -1,147 +1,138 @@
-		var money = 1000;
-		var house_price = 1044303;
-		var cache = {timestamp : 0, value : -1};
 
-		function money_avo(money){
-			var avo_perkg = 12.95;
-			var kg = money/avo_perkg;
-			var no_avos = Math.floor(kg/0.17);
+var house_price = 1044303;
+var cache = {timestamp : 0, value : -1};
 
-			return no_avos;
-		}
+function money_avo(money){
+	var avo_perkg = 12.95;
+	var kg = money/avo_perkg;
+	var no_avos = Math.floor(kg/0.17);
 
-		function money_house(money){
-			house_percent = (money*100)/1044303;
-			house_percent = house_percent.toFixed(1);
+	return no_avos;
+}
 
-			return house_percent;
-		}
+function money_house(money){
+	house_percent = (money*100)/1044303;
+	house_percent = house_percent.toFixed(1);
 
-		function avo_money(no_avos){
-			money = (no_avos*0.17)*12.95;
-			money = Math.round(money);
+	return house_percent;
+}
 
-			return money;
-		}
+function avo_money(no_avos){
+	money = (no_avos*0.17)*12.95;
+	money = Math.round(money);
 
-
-		function house_money(house_percent){
-
-			money = (house_percent/100)*house_price;
-			return Math.round(money);
-		}
-
-		function money_input(){
-			var money = 1000;
-			no_avos = money_avo(money);
-			house_percent = money_house(money);
-
-			return [no_avos, house_percent];
-		}
-
-		function avo_input(){
-			var no_avos = 100000;
-			money = avo_money(no_avos);
-			house_percent = money_house(money);
-
-			return [money,house_percent];
-		}
-
-		function house_input(){
-
-			var house_percent = 30;
-			money = house_money(house_percent);
-			no_avos = money_avo(money);
-
-			return [money, no_avos];
-		}
-
-		console.log(house_input());
+	return money;
+}
 
 
-		function money_bitcoin(amountToBeConverted, display) {
+function house_money(house_percent){
 
-				var bitcoinValue;
+	money = (house_percent/100)*house_price;
+	return Math.round(money);
+}
 
-				console.log("1");
-				$.ajax({
-					url:'/bitcoin',
-					type: 'GET',
-					success: function (data, textstatus) {
-					console.log("2");
-						var bitcoinInfo = JSON.parse(body);
-						console.log("JSON: " + body);
-						console.log("bitcoinInfo: " + bitcoinInfo);
-						var roundedBitCoinValue = Math.round(bitcoinInfo[0].value);
+function money_input(){
+	var money = 1000;
+	no_avos = money_avo(money);
+	house_percent = money_house(money);
 
-						console.log("from server: " + roundedBitCoinValue);
-						//bitcoinValue = roundedBitCoinValue;
-						bitcoinValue = roundedBitCoinValue;
-						var numBitcoins = amountToBeConverted/bitcoinValue;
-						display(numBitcoins);
-					}
-				});
+	return [no_avos, house_percent];
+}
 
+function avo_input(){
+	var no_avos = 100000;
+	money = avo_money(no_avos);
+	house_percent = money_house(money);
 
+	return [money,house_percent];
+}
 
-		}
+function house_input(){
 
-	function display(numBitcoins) {
-		jQuery( "#bitcoinInput" ).text(numBitcoins);
-		console.log("displaying correctly ")
+	var house_percent = 30;
+	money = house_money(house_percent);
+	no_avos = money_avo(money);
+
+	return [money, no_avos];
+}
+
+console.log(house_input());
+
+function conversion() {
+	var httpRequest;
+	httpRequest = new XMLHttpRequest();
+
+	if (!httpRequest) {
+		alert('Giving up :( Cannot create an XMLHTTP instance');
+		return false;
 	}
+	httpRequest.onreadystatechange = display;
+	httpRequest.open('GET', 'https://nzbcx.com/api/market/BTCNZD/LTP');
+	httpRequest.send();
+}
+
+function display(numBitcoins) {
+	$( "#bitcoin" ).text(numBitcoins + " Bitcoins" );
+	console.log("displaying correctly ")
+}
+
+function displayAvo(noAvo) {
+	$( "#avo" ).text(noAvo + " Avocados" );
+	console.log("displaying correctly ")
+}
+
+function displayHouse(house) {
+	$( "#house" ).text(house + "% of a house" );
+	console.log("displaying correctly ")
+}
+
+function displayAmount(useramount) {
+	$( "#useramount" ).text("With $" + useramount + " you can buy..." );
+	console.log("displaying correctly ")
+}
+
+function getInput() {
+	var regex = /searchbar=(.+)&/;
+	var url = window.location.href;
+	var amount = parseInt(url.match(regex)[1]);
+
+	console.log("get input test");
+	console.log(amount)
+	displayAmount(amount)
+	getBitCoinInfo(amount)
+	var no_avo = money_avo(amount)
+	displayAvo(no_avo)
+	var house = money_house(amount)
+	displayHouse(house)
+}
+
+function conversion(money_in) {
+	var convertedAvo = money_avo(money_in);
+	var convertedHouse = money_house(money_in);
+	var results = [money_in, convertedAvo, convertedHouse];
+	return results;
+}
+
+function getBitCoinInfo(amountToBeConverted) {
+
+		var bitcoinValue;
 
 
-	function getInput() {
-		console.log("get input test");
-		var money_in = jQuery("#moneyIn").val();
-		var userResults = conversion(money_in);
-		getBitCoinInfo(money_in);
-		console.log(money_in);
-		return userResults;
-	}
+		jQuery.getJSON('/bitcoin', function (data) {
+				console.log("JSON: " + data);
+				var roundedBitCoinValue = data;
 
-	function conversion(money_in) {
-		var convertedAvo = money_avo(money_in);
-		var convertedHouse = money_house(money_in);
-		var results = [money_in, convertedAvo, convertedHouse];
-		return results;
-	}
+				console.log("from server: " + roundedBitCoinValue);
+				//bitcoinValue = roundedBitCoinValue;
+				bitcoinValue = parseInt(roundedBitCoinValue);
+				var numBitcoins = (amountToBeConverted/bitcoinValue);
+				console.log(typeof(amountToBeConverted))
+				console.log(bitcoinValue)
+				display(numBitcoins.toPrecision(5));
+		});
 
-	function getBitCoinInfo(amountToBeConverted) {
+}
 
-			var bitcoinValue;
-
-
-			jQuery.getJSON('/bitcoin', function (data) {
-					console.log("JSON: " + data);
-					var bitcoinInfo = data;
-					console.log("bitcoinInfo: " + bitcoinInfo);
-					var roundedBitCoinValue = Math.round(bitcoinInfo[0].value);
-
-					console.log("from server: " + roundedBitCoinValue);
-					//bitcoinValue = roundedBitCoinValue;
-					bitcoinValue = roundedBitCoinValue;
-					var numBitcoins = amountToBeConverted/bitcoinValue;
-					display(numBitcoins);
-			});
-
-	}
-
-	function bitcoinRequest() {
-		var httpRequest;
-		httpRequest = new XMLHttpRequest();
-
-		if (!httpRequest) {
-			alert('Giving up ?? Cannot create an XMLHTTP instance');
-			return false;
-		}
-		httpRequest.onreadystatechange = display;
-		httpRequest.open('GET', 'https://nzbcx.com/api/market/BTCNZD/LTP');
-		httpRequest.send();
-	}
-
-	var userResults = ["stuff", "12341"]
-	var div = jQuery( "div" )[ 0 ];
-	jQuery( "#userInput" ).text(userResults[0]);
-	jQuery( "#avocadoInput" ).text(userResults[1]);
+$(document).ready(function(){
+	getInput();
+});
